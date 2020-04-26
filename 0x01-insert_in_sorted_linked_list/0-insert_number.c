ce58ce18c9c4f07968b199ada80e5ffa7a2afddf
jfkquality <jfkquality@gmail.com>
2020-04-26 15:12:51 -0400

    Task 0 Fix. Account for adding at start and end.

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -15,31 +15,43 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *current;
 
 	current = *head;
+	prev = *head;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-
 	new->n = number;
-
 	if (*head == NULL)
 		*head = new;
 	else
 	{
 		while (current->next != NULL)
 		{
-			if (current->n < number)
+			/* printf("in while loop %i \n", current->n); */
+			if (current->n > number)
+				break;
+			else
 			{
+				/* printf("if current < num\n"); */
 				prev = current;
 				current = current->next;
 			}
-			else
-			{
-				new->next = current;
-				prev->next = new;
-			}
 		}
-		if (current->next == NULL)
+		if (prev == *head)
+		{
+			/* printf("prev is NULL %i. \n", prev->n); */
+			new->next = current;
+			*head = new;
+			/* return (new); */
+		}
+		else if (current->next != NULL)
+		{
+			new->next = current;
+			prev->next = new;
+			/* return (new); */
+		}
+		/* } */
+		else
 		{
 			new->next = NULL;
 			current->next = new;
--- 0x01-insert_in_sorted_linked_list/0-main.c
+++ 0x01-insert_in_sorted_linked_list/0-main.c
@@ -13,11 +13,11 @@ int main(void)
 	listint_t *head;
 
 	head = NULL;
-	add_nodeint_end(&head, 0);
 	add_nodeint_end(&head, 1);
 	add_nodeint_end(&head, 2);
 	add_nodeint_end(&head, 3);
 	add_nodeint_end(&head, 4);
+	add_nodeint_end(&head, 5);
 	add_nodeint_end(&head, 98);
 	add_nodeint_end(&head, 402);
 	add_nodeint_end(&head, 1024);
@@ -25,7 +25,11 @@ int main(void)
 
 	printf("-----------------\n");
 
-	insert_node(&head, -1);
+	insert_node(&head, 27);
+
+	print_listint(head);
+
+	insert_node(&head, 28);
 
 	print_listint(head);
 
