add4bf2bb1c76b1164211ef518dd4468f14def7d
jfkquality <jfkquality@gmail.com>
2020-05-12 15:55:13 -0400

    Task 0 bugfix: NOW use H instead of 1.

--- 0x03-minimum_operations/0-main.py
+++ 0x03-minimum_operations/0-main.py
@@ -8,5 +8,14 @@ minOperations = __import__('0-minoperations').minOperations
 n = 4
 print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
 
+n = 7
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 9
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
 n = 12
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 13
 print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
