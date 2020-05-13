6da7dff43ea6d6246b1691e9b74e238b064a558c
jfkquality <jfkquality@gmail.com>
2020-05-13 00:18:24 -0400

    Task 0 debug: Linter errors on main.py.

--- 0x03-minimum_operations/0-main.py
+++ 0x03-minimum_operations/0-main.py
@@ -6,19 +6,25 @@ Main file for testing
 minOperations = __import__('0-minoperations').minOperations
 
 n = 4
-print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+print("Min number of operations to reach {} characters: {}"
+      .format(n, minOperations(n)))
 
 n = 7
-print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+print("Min number of operations to reach {} characters: {}"
+      .format(n, minOperations(n)))
 
 n = 9
-print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+print("Min number of operations to reach {} characters: {}"
+      .format(n, minOperations(n)))
 
 n = 1
-print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+print("Min number of operations to reach {} characters: {}"
+      .format(n, minOperations(n)))
 
 n = 2
-print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+print("Min number of operations to reach {} characters: {}"
+      .format(n, minOperations(n)))
 
 n = 9999
-print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+print("Min number of operations to reach {} characters: {}"
+      .format(n, minOperations(n)))
