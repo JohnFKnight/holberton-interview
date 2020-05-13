4cec32558d30ab2aa3efbcd0426ccb0476916005
jfkquality <jfkquality@gmail.com>
2020-05-13 14:20:44 -0400

    Task 0 refactor: Try 2nd version again with correct doc strings.

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,47 +1,24 @@
 #!/usr/bin/python3
-""" Fin min ops."""
 
+""" Find min num ops"""
 
-# Tail recursive. -Will?
-# Least num of commands to go from x=1 to n
-
-
-
-from sys import setrecursionlimit
 
 setrecursionlimit(10**6)
 
-def minop(n, x="H", cp="H", acc=0):
-    """ Calc min ops """
-
-    if (n == 1 or n < 0):
-        return 0
-    if (len(x) == n):
-        return acc + 1
-    if (len(x) > n):
-        return -1
-
-    # option copy paste
-    y = minop(n, x*2, x, acc+2)
-    # print("y=", y)
-
-    # option paste
-    z = minop(n, x+cp, cp, acc+1)
-
-    # print("z=", z)
-    # print("####" * 3)
-    if (y != -1 and y <= z):
-        return int(y)
-    return int(z)
-
-# print(minop(10))
-
 
 def minOperations(n):
-    """Driver code."""
+    """Calc number of ops."""
+    acc = 0
 
-    # if (__name__ == "__main__"):
-    ops = minop(n)
-    if (ops == -1):
+    if n < 2 or type(n) is not int:
         return 0
-    return ops
+
+    for i in range(2, n):
+        while (n % i) == 0:
+            acc += i
+            n = n / i
+
+    if acc == 0:
+        acc = n
+
+    return acc
