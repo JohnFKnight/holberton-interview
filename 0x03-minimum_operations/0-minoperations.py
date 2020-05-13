a5901663c52a831e9a3d19b71ffc8ede5472156a
jfkquality <jfkquality@gmail.com>
2020-05-13 01:46:14 -0400

    Task 0 refactor. Revert to previous version.

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -10,17 +10,12 @@ setrecursionlimit(10**6)
 
 def minOperations(n):
     """Calc number of ops."""
-    acc = 0
-
-    if n < 2 or type(n) is not int:
-        return 0
-
-    for i in range(2, n):
-        while (n % i) == 0:
-            acc += i
-            n = n / i
-
-    if acc == 0:
-        acc = n
-
-    return acc
+    ops = 0
+    i = 2
+    while i <= n:
+        if n % i == 0:
+            ops += i
+            n /= i
+        else:
+            i += 1
+    return ops
