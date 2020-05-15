9cb927fe99ea9a427ce1a27d56998152beaf190e
jfkquality <jfkquality@gmail.com>
2020-05-15 12:34:23 -0400

    Task 0 bugfix. Linter err. Cleanup debug comments.

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -7,21 +7,10 @@ def minOperations(n):
     """Calc number of ops."""
     acc = 0
 
-    # if n < 2 or type(n) is not int:
-    #     return 0
-    # i = 2
-    # while i <= n:
     for i in range(2, n + 1):
         if n == 1:
             break
-        # print("i1 ", i, "n1 ", n, "acc1 ", acc)
         while n % i == 0:
-        # if n % i == 0:
-            # print("i2 ", i, "n2 ", n, "acc2 ", acc)
             acc += i
             n = n / i
-            # print("i3 ", i, "n3 ", n, "acc3 ", acc)
-        # else:
-        #     i += 1
-
     return acc
