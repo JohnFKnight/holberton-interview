0a7afa1278c10619fb54803c37248b628e533840
jfkquality <jfkquality@gmail.com>
2020-06-24 16:58:48 -0400

    Task 0 bugfix: Add test for >= 0 .

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -6,6 +6,6 @@ Determine if a given data set represents a valid UTF-8 encoding.
 
 def validUTF8(data):
     for num in data:
-        if num > 255:
-            return (False)
-    return (True)
+        if num > 0 and num < 256:
+            return (True)
+    return (False)
