ae561b8dc27e175d77b9d84797c27fe9c9c95ea7
jfkquality <jfkquality@gmail.com>
2020-06-24 17:44:57 -0400

    Task 0: bufix. Add test if num is int.

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -6,6 +6,6 @@ Determine if a given data set represents a valid UTF-8 encoding.
 
 def validUTF8(data):
     for num in data:
-        if num < 0 or num > 255:
+        if isinstance(num, int) and (num < 0 or num > 255):
             return (False)
     return (True)
