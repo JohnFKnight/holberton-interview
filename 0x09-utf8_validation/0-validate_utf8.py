09b3eea451f74cbb209265f131631db68b1ddd0a
jfkquality <jfkquality@gmail.com>
2020-06-24 17:10:45 -0400

    Task 0: bufix. Fix if stmt; use or not and.

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -6,6 +6,6 @@ Determine if a given data set represents a valid UTF-8 encoding.
 
 def validUTF8(data):
     for num in data:
-        if num > 0 and num < 256:
-            return (True)
-    return (False)
+        if num < 0 or num > 255:
+            return (False)
+    return (True)
