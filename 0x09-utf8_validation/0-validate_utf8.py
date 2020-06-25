6065c1a951c4c07ca6284782f94dc8647cff0d70
jfkquality <jfkquality@gmail.com>
2020-06-25 01:51:05 -0400

    Task 0, bugfix: Rewrite using bit shifting and masking.

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -3,11 +3,21 @@
 Determine if a given data set represents a valid UTF-8 encoding.
 """
 
-
 def validUTF8(data):
-    """ Test each num in list if UTF-8."""
-
-    for num in data:
-        if (num & (1 << 7) != 0) and num > 127:
+    # print(len(data))
+    mask1 = 1 << 7
+    mask2 = 1 << 6
+    if len(data) == 1:
+        if num & mask1:
+            return False
+        else:
+            return True
+    for i, num in enumerate(data):
+        if num < 128:
+            if num & mask1 != 0:  #See how many bytes it is; i.e. MSB's = 1
+                return False
+            else:
+                return True
+        elif not (num & mask1 and not (num & mask2)):
             return (False)
     return (True)
