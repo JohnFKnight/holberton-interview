b9c76d5f43c7b27b3c497499133132e769e53b6b
jfkquality <jfkquality@gmail.com>
2020-06-24 20:26:01 -0400

    Task 0: bufix. Add test if MSB is zero.

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -6,6 +6,6 @@ Determine if a given data set represents a valid UTF-8 encoding.
 
 def validUTF8(data):
     for num in data:
-        if isinstance(num, int) and (num < 0 or num > 255):
+        if (num & (1 << 7) != 0) or (num < 0 or num > 255):
             return (False)
     return (True)
