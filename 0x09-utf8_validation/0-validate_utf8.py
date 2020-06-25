92fb4df86f39463dd3ba976ad1e8fda6b8ad0f64
jfkquality <jfkquality@gmail.com>
2020-06-24 23:05:59 -0400

    Task 0, bugfix: Add doc string after func declaration.

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -5,7 +5,9 @@ Determine if a given data set represents a valid UTF-8 encoding.
 
 
 def validUTF8(data):
+    """ Test each num in list if UTF-8."""
     for num in data:
-        if (num & (1 << 7) != 0) or num > 127:
-            return (False)
-    return (True)
+        for num in data:
+            if (num & (1 << 7) != 0) and num > 127:
+                return (False)
+        return (True)
