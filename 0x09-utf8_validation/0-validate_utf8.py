8ceb235df72d8f7e470cf77307d8aceba4e8725a
jfkquality <jfkquality@gmail.com>
2020-06-24 21:07:42 -0400

    Task 0: bugfix. Put back range test but only up to 127.

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -6,6 +6,6 @@ Determine if a given data set represents a valid UTF-8 encoding.
 
 def validUTF8(data):
     for num in data:
-        if (num & (1 << 7) != 0):
+        if (num & (1 << 7) != 0) and num > 127:
             return (False)
     return (True)
