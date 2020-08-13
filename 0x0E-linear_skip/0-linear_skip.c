f56b0d8cfc90488cb1d030c9f782df913a77911e
jfkquality <jfkquality@gmail.com>
2020-08-13 00:22:00 +0000

    Task 0, bugfix: Linter errors.

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -8,7 +8,9 @@ void free_skiplist(skiplist_t *list);
  * linear_skip - Find value in express lane of the linked list
  *
  * @list: Pointer to the head node of the list
- * @size: Number of nodes in the list
+ * @value: Number to search nodes in the list
+ *
+ * Return: pointer to found node
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
@@ -17,7 +19,7 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	skiplist_t *current;
 
 	if (list == NULL)
-		return NULL;
+		return (NULL);
 
 	current = list;
 	while (current)
@@ -25,7 +27,7 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 		printf("Value checked at index [%lu] = [%d]\n",
 		      current->express->index, current->express->n);
 		if (current == list && value < current->n)
-			return NULL;
+			return (NULL);
 		if (value >= current->n && value <= current->express->n)
 		{
 			printf("Value found between indexes [%lu] and [%lu]\n",
@@ -33,38 +35,18 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 			while (current->index <= current->express->index)
 			{
 				if (value == current->n)
-					return current;
-				else
-				{
-					/* printf("check #2\n"); */
-					printf("Value checked at index [%lu] = [%d]\n",
-					      current->index, current->n);
-					/* printf("idx1 %lu. idx2 %lu \n", current->index, current->next->index); */
-					current = current->next;
-				}
+					return (current);
+				/* printf("check #2\n"); */
+				printf("Value checked at index [%lu] = [%d]\n",
+				       current->index, current->n);
+				/*
+				 *printf("idx1 %lu. idx2 %lu \n",
+				 * current->index, current->next->index);
+				 */
+				current = current->next;
 			}
 		}
 		current = current->express;
 	}
-	return NULL;
+	return (NULL);
 }
-	/* 		if (value == current->n) */
-	/* 		if (current == list && value = list->n) */
-	/* 		{ */
-	/* 			print("Value found between indexes [%lu] and [%lu]\n", list->index, list->express->index); */
-	/* 			return current; */
-	/* 		} */
-	/* 		else */
-	/* 			return NULL; */
-	/* 	} */
-	/* 	if (value < list->express->n) */
-	/* 	{ */
-	/* 		print("Value found between indexes [%lu] and [%lu]\n", list->index, list->express->index); */
-	/* 			current = current->next; */
-	/* 	} */
-	/* 	else { */
-	/* 		if (value < list->n) */
-	/* 		{ */
-	/* 			if (value < list->express->n) */
-	/* 				print("Value found between indexes [%lu] and [%lu]\n", list->index, list->express->index); */
-	/* } */
