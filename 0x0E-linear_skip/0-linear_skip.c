cd23521c7c6147db2ca881c4e48c535432039a29
jfkquality <jfkquality@gmail.com>
2020-08-13 03:28:04 +0000

    Task 0, bugfix: Chenaged inner while to while curr-nxt. Add ocde at end for edge case.

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -14,39 +14,47 @@ void free_skiplist(skiplist_t *list);
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	/* const size_t step = sqrt(size); */
-	/* size_t i; */
 	skiplist_t *current;
+	skiplist_t *last;
 
 	if (list == NULL)
 		return (NULL);
 
 	current = list;
-	while (current)
+	while (current->express)
 	{
 		printf("Value checked at index [%lu] = [%d]\n",
 		      current->express->index, current->express->n);
 		if (current == list && value < current->n)
 			return (NULL);
-		if (value >= current->n && value <= current->express->n)
+		if ((value >= current->n && value <= current->express->n))
 		{
 			printf("Value found between indexes [%lu] and [%lu]\n",
 			      current->index, current->express->index);
-			while (current->index <= current->express->index)
+			while (current->next)
 			{
 				if (value == current->n)
 					return (current);
-				/* printf("check #2\n"); */
 				printf("Value checked at index [%lu] = [%d]\n",
 				       current->index, current->n);
-				/*
-				 *printf("idx1 %lu. idx2 %lu \n",
-				 * current->index, current->next->index);
-				 */
 				current = current->next;
 			}
 		}
 		current = current->express;
 	}
+	last = current;
+	while (current->next)
+		current = current->next;
+	printf("Value found between indexes [%lu] and [%lu]\n",
+	       last->index, current->index);
+	current = last;
+	while (current->next)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+		       current->index, current->n);
+		current = current->next;
+	}
+	printf("Value checked at index [%lu] = [%d]\n",
+	       current->index, current->n);
 	return (NULL);
 }
