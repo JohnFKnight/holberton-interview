d4e525276b2153b242f3ea499405a8bf7c4e039d
jfkquality <jfkquality@gmail.com>
2020-09-09 22:39:07 +0000

    Task 0. Bugfix: Add check for more searching the left. Kind of working.

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -12,7 +12,9 @@
 int advanced_binary(int *array, size_t size, int value)
 {
 	size_t m, x;
-	signed int found;
+	signed int found, left;
+
+	left = 1;
 
 	if (size == 0)
 	{
@@ -29,18 +31,32 @@ int advanced_binary(int *array, size_t size, int value)
 		m = size / 2;
 	if (array[m] == value)
 	{
-		found = m;
+		/* found = m; */
+		if (left && m> 0)
+		{
+
+			found = advanced_binary(array, m, value);
+			/* printf("Searching in array: "); */
+			/* for (x = 0; x < size - 1; x++) */
+			/* 	printf("%d, ", array[x]); */
+			/* printf("%d\n", array[x]); */
+		}
+
+		else
+			found = m;
 	}
 	else if (array[m] < value)
 	{
 		/* Upper half. So search in upper half of the current array */
 		/* with new length of the upper half. */
+		/* First found in upper/right is the first so return idx */
 		found = advanced_binary(array + m + 1, size - m - 1, value);
 
 		if (found != (-1))
 		{
 			/* Since we've found a key, need to offset it to make */
 			/* valid in the current search scope */
+			left = 0;
 			found += m + 1;
 		}
 	}
@@ -48,6 +64,7 @@ int advanced_binary(int *array, size_t size, int value)
 	{
 		/* Lower half, there is no need to offset the array.  */
 		/* New array length is equal to the current middle point. */
+		left = 1;
 		found = advanced_binary(array, m, value);
 	}
 	return (found);
