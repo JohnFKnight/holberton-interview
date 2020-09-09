19d28cdac1be181e0b028c870d8311bfabe56635
jfkquality <jfkquality@gmail.com>
2020-09-09 21:01:49 +0000

    Task 0. Init commit. Working?

---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef _SEARCHALGOS_
+#define _SEARCHALGOS_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int advanced_binary(int *array, size_t size, int value);
+
+#endif  /* _SEARCHALGOS_ */
