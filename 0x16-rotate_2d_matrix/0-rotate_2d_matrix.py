df1f636657b329a09cbefbe128a3161d5ea41344
jfkquality <jfkquality@gmail.com>
2020-10-08 09:48:37 +0000

    503, Task 0, Refactor more: Remove most debugging comments.

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,44 +1,34 @@
 #!/usr/bin/python3
 """ Rotate a matrix. """
 
+
 def printarr(arr):
+    """Print matrix."""
     for row in arr:
         print(row)
 
+
 def rotate_2d_matrix(m):
     """ Roatate a matrix."""
-    printarr(m)
-    print('\n')
-    # m = rez
-    # for x in range(3):
-    # for row in m:
-        # row.reverse()
-        # print("Row reversed: ", row)
+    # printarr(m)
+    # print('\n')
+
     m = [[m[j][i] for j in range(len(m))] for i in range(len(m[0]))]
-    # print("row", row)
-    print("print m function")
-    printarr(m)
-        # print("print rez loop")
+
+    # print("print m after transpose")
+    # printarr(m)
+    # print('\n')
 
     for row in m:
         row.reverse()
-        # print("rez: ", rez)
-    print("After reverse")
-    printarr(m)
-        # print( "Before reverse")
-        # printarr(rez)
-        # rez.reverse()
-        # m = rez
-        # print("After reverse")
-        # printarr(rez)
-        # print('\n')
-        # print('\n')
-        # print(rez)
-    # for a, row in enumerate(rez):
-    #     # print(row)
+
+    # print("After reverse")
+    # printarr(m)
+    # print('\n')
+
+    # for a, row in enumerate(m):
     #     for b, col in enumerate(row):
     #         m[a][b] = col
 
-    # for row in m:
-    #     print(row)
+    # printarr(m)
     # print('\n')
